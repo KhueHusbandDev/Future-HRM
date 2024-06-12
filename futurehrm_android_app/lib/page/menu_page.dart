00e502f5b7c1565f8aec3f848d17a8d38b9e6184
ITFakeFace <dthung6604@gmail.com>
2024-06-12 23:02:10 +0700

    Add Create with Image in Server and add Hive to Flutter, init Menu Page

--- futurehrm_android_app/lib/page/menu_page.dart
+++ futurehrm_android_app/lib/page/menu_page.dart
@@ -1,21 +1,74 @@
 import 'package:flutter/material.dart';
+import 'package:futurehrm_android_app/models/staff.dart';
+import 'package:hive/hive.dart';
 
 class MenuPage extends StatefulWidget {
-  int authId;
+  static String routeName = "/menu";
 
-  MenuPage({super.key, required this.authId});
+  MenuPage({super.key});
 
   @override
-  State<MenuPage> createState() => _MenuPageState(authId);
+  State<MenuPage> createState() => _MenuPageState();
 }
 
 class _MenuPageState extends State<MenuPage> {
-  late int authId;
+  late Staff currentAuth;
+  var actions = [
+    {"name": "Check In", "image": "none"},
+    {"name": "Attendance", "image": "none"},
+    {"name": "Payroll", "image": "none"},
+    {"name": "Request Leave", "image": "none"},
+  ];
 
-  _MenuPageState(int authId);
+  void getCurrentAuth() async {
+    var box = await Hive.openBox("Auth");
+    currentAuth = box.get("CurrentAuth");
+    print(currentAuth);
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    getCurrentAuth();
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Menu"),
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+          child: Column(
+            children: [
+              GridView.builder(
+                shrinkWrap: true,
+                padding: EdgeInsets.all(5),
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 3,
+                  mainAxisSpacing: 40,
+                  crossAxisSpacing: 24,
+                  // width / height: fixed for *all* items
+                  childAspectRatio: 4 / 3,
+                ),
+                // return a custom ItemCard
+                itemBuilder: (context, i) {
+                  var item = actions[i];
+                  return Card(
+                    color: Colors.white,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                    elevation: 5,
+                    child: Center(
+                      child: Text(item["name"]!),
+                    ),
+                  );
+                },
+                itemCount: actions.length,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
